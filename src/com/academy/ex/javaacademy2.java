3b0f1a4c2f03d81ebd3e74fbebfb08b05e2ac5bc
Administrator <Administrator@192.168.0.84>
2020-02-26 09:27:46 +0900

    02226

--- src/com/academy/ex/javaacademy2.java
+++ src/com/academy/ex/javaacademy2.java
@@ -4,48 +4,84 @@ public class javaacademy2 {
 
 	public static void main(String[] args) {
 		//안녕 3번 찎기
-		int i;
-		for(i=1;i<=100;i++) {
-			System.out.println(i);
+//		int i;
+//		for(i=1;i<=100;i++) {
+//			System.out.println(i);
+//		}
+//		int a=1;
+//		while(a<=100) {
+//			System.out.println(a);
+//			a++;
+//		}
+//		
+//		
+//		int i1;
+//		for(i1=5;i1<9;i1++) {
+//			System.out.println(i1);
+//		}
+//		int b=5;
+//		while(b<9) {
+//			System.out.println(b);
+//			b++;
+//		}
+//		
+//		
+//		int i2;
+//		for(i2=13;i2<23;i2++) {
+//			System.out.println(i2);
+//		}
+//		int c=13;
+//		while(c<23) {
+//			System.out.println(c);
+//			c++;
+//		}
+//		
+//		
+//		//1-10합
+//		int i3;
+//		int sum=0;
+//		for(i3=1;i3<=10;i3++){
+//			sum=sum+i3;		
+//		}
+//		System.out.println(sum);
+//		int d=1;
+//		int sum1=0;
+//		while(d<=10) {
+//			sum1+=d;
+//			d++;
+//		}
+//		System.out.println(sum1);
+//		
+//		
+//		int i4;
+//		for(i4=100;i4>=1;i4--) {
+//			System.out.println(i4);
+//		}
+//		int e=100;
+//		while(e>=1) {
+//			System.out.println(e);
+//			e--;
+			
+			
+		int i5;
+		for(i5=1;i5<=100;i5++) {
+			if(i5%2==0) {
+				System.out.println(i5);
+			}
 		}
-		int a=1;
-		while(a<=100) {
-			System.out.println(a);
-			a++;
-		}
-		int i1;
-		for(i1=5;i1<9;i1++) {
-			System.out.println(i1);
-		}
-		int b=5;
-		while(b<9) {
-			System.out.println(b);
-			b++;
-		}
-		int i2;
-		for(i2=13;i2<23;i2++) {
-			System.out.println(i2);
-		}
-		int c=13;
-		while(c<23) {
-			System.out.println(c);
-			c++;
-		}
-		//1-10합
-		int i3;
-		int sum=0;
-		for(i3=1;i3<=10;i3++){
-			sum=sum+i3;		
-		}
-		System.out.println(sum);
-		int d=1;
-		int sum1=0;
-		while(d<=10) {
-			sum1+=d;
-			d++;
-		}
-		System.out.println(sum1);	
+//		int f=0;
+//		while(f<100) {
+//			f++;
+//			if(f%2==0) {
+//				System.out.println(f);
+//				
+//			}
+//			
+//		}
+//	
 		
+		
+	
 	}
 
 }
---
+++ src/com/academy/ex/javaacademy3.java
@@ -0,0 +1,49 @@
+package com.academy.ex;
+
+public class javaacademy3 {
+
+	public static void main(String[] args) {
+		//if 조건문
+		/*if(조건) {
+			//조건이 참일때 실행
+		}
+		else {
+			//조건이 거짓일떄 싱행
+		}
+		조건식으로 올 수 있는것은 실행결과가 boolean인 경우
+		(true false 비교연산자 논리연산자)
+		
+		if(true) {
+			System.out.println("true");
+		}
+		else {
+			System.out.println("false");				
+		}
+		
+		
+		if(true) 
+			System.out.println("true");
+		
+		else 
+			System.out.println("false");//실행부가 1줄이면 중괄호 생략가능
+		System.out.println("하이");//이 코드는 if문 밖에있는 코드
+		
+		//else부분 자체를 생략가능
+		if(10<40) { 
+			System.out.println("true");
+		}
+		*/
+		int a=10;
+		if(a==10) {
+			System.out.println("true");
+		}
+		if(a!=17) {//a가 10이 아니면
+			System.out.println("true");
+		}
+		
+		
+		
+
+	}
+
+}
