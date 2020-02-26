047ac364a85a200cdb9efe86557796509430e666
Administrator <Administrator@192.168.0.84>
2020-02-26 16:34:25 +0900

    02

--- src/com/academy/ex/javaacademy7.java
+++ src/com/academy/ex/javaacademy7.java
@@ -1,53 +1,71 @@
 package com.academy.ex;
 
+ 
+
 public class javaacademy7 {
 
+ 
+
 	public static void main(String[] args) {
-		//1부터 100
-		int i;
-		for(i=1;i<=100;i++) {
-			System.out.println(i);
-		}
-		int a=1;
-		while(a<=100) {
-			System.out.println(a);
-			a++;
-		}
-		//5부터8까지
-		int i1;
-		for(i1=5;i1<9;i1++) {
-			System.out.println(i1);
-		}
-		int b=5;
-		while(b<9) {
-			System.out.println(b);
-			b++;
-		}
-		//13부터 22까지
-		int i2;
-		for(i2=13;i2<23;i2++) {
-			System.out.println(i2);
-		}
-		int c=13;
-		while(c<23) {
-			System.out.println(c);
-			c++;
-		}
-		//1-10합
-		int i3;
-		int sum=0;
-		for(i3=1;i3<=10;i3++){
-			sum=sum+i3;		
-		}
-		System.out.println(sum);
-		int d=1;
-		int sum1=0;
-		while(d<=10) {
-			sum1+=d;
-			d++;
-		}
-		System.out.println(sum1);	
+
+		java.util.Scanner sc=new java.util.Scanner(System.in);
+
 		
+
+		  int max,min=0;
+
+		  int i;
+
+		  max=Integer.MIN_VALUE;
+
+		  min=Integer.MAX_VALUE;
+
+		  
+
+		  for(i=0;i<5;i++) {
+
+		  System.out.println("수입력");
+
+		 i=Integer.parseInt(sc.nextLine());
+
+		  }
+
+		  if(max<i){
+
+		  max=i;
+
+		  }
+
+		  if(min>i){
+
+		  }
+
+		  System.out.println("큰수:"+max+"작은수:"+min);
+
+		 
+
+		
+
+			
+
+			
+
+	
+
+		
+
+		
+
+		
+
+	
+
+ 
+
 	}
+
+ 
+
+ 
 
 }
